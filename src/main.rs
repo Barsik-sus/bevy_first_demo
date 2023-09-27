9832855f4663013b5a1bc6110ad8c1cc1351a0c3
Barsik <bogdan.balushkin5@gmail.com>
2023-09-27 12:54:53 +0300

    people with `age` field

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Component, Clone, Copy)]
+struct Age(u8);
+
 fn add_people(mut commands: Commands) {
     commands.spawn_batch([
-        (Person, Name("Igor".into())),
-        (Person, Name("Peter".into())),
-        (Person, Name("Oleg".into())),
+        (Person, Name("Igor".into()), Age(13)),
+        (Person, Name("Peter".into()), Age(8)),
+        (Person, Name("Oleg".into()), Age(100)),
     ])
 }
 
@@ -18,9 +21,9 @@ fn hello_world() {
     println!("hello world");
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(query: Query<(&Name, &Age), With<Person>>) {
+    for (name, age) in &query {
+        println!("hello {}, {} y.o.!", name.0, age.0);
     }
 }
 
