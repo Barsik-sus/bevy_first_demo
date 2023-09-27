6d1772deb1fb186984a11e217f2f0e673bab6885
Barsik <bogdan.balushkin5@gmail.com>
2023-09-27 12:44:26 +0300

    greet people

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn_batch([
+        (Person, Name("Igor".into())),
+        (Person, Name("Peter".into())),
+        (Person, Name("Oleg".into())),
+    ])
+}
+
 fn hello_world() {
     println!("hello world");
 }
 
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
         .run();
 }
