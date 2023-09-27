cc09139b76cf21c5bc5aab2613640a76ac36c6f6
Barsik <bogdan.balushkin5@gmail.com>
2023-09-27 13:15:41 +0300

    first resource

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,15 @@ fn add_people(mut commands: Commands) {
     ])
 }
 
-fn hello_world() {
-    println!("hello world");
-}
-
-fn greet_people(query: Query<(&Name, &Age), With<Person>>) {
-    for (name, age) in &query {
-        println!("hello {}, {} y.o.!", name.0, age.0);
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<(&Name, &Age), With<Person>>
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for (name, age) in &query {
+            println!("hello {}, {} y.o.!", name.0, age.0);
+        }
     }
 }
 
@@ -32,11 +34,15 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
+            .add_systems(Update, greet_people);
 
     }
 }
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn main() {
     App::new()
